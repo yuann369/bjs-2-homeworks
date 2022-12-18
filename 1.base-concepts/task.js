c73399d6b344c2a1eea24c6a33c380dbc22f4fe9
Anna Yurasova <anna.yurasova@ssi-ru.local>
2022-12-18 19:23:07 +0400

    change task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b,2)-4*a*c;
+  if (d==0){
+    arr = -b/(2*a);
+  }
+  if (d>0){
+    arr[(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+  }
+  else {
+    arr = 0;
+  }
   return arr;
 }
 
