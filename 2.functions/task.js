94c6d5e00d73e2ab7bc293b461d7477abe96dc01
Anna Yurasova <anna.yurasova@ssi-ru.local>
2022-12-20 01:23:37 +0400

    tasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,44 @@
-function getArrayParams(...arr) {
+// Задание 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg=0;
 
+  for (let i=0; i<arr.length; i++){
+    if (arr[i]<min){
+      min = arr[i];
+    }
+    if (arr[i]>max){
+      max = arr[i];
+    }
+    sum+=arr[i];
+  }
+  avg = Number((sum/arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
-
+// Задание 2
+function worker(arr) {
+  let sum=0;
+  for (let i=0; i<arr.length; i++){
+    sum+=arr[i];
+  }
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+  let max=-Infinity;
+  for (let i=0; i<arrOfArr.length; i++){
+    if (func(arrOfArr[i])>max){
+      max=func(arrOfArr[i]);
+    }
+  }
+  return max;
 }
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+  let difference = getArrayParams(arr).max - getArrayParams(arr).min;
+  return (Math.abs(difference));
 }
