03389037250f9d2c9a8dbf2554745621f13081de
anna.yurasova <anna.yurasova@ssi-schafer.ru>
2023-05-23 20:59:12 +0400

    add task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject=subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+        return;
+      } else {
+        this.marks.push(...marksToAdd);
+      }
 }
 
 Student.prototype.getAverage = function () {
   
-}
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+    let sum = 0;
+    this.marks.forEach(function(elem) {
+      sum += elem;
+    });
+    return sum / this.marks.length
+      }
 
-Student.prototype.exclude = function (reason) {
   
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
